a6b6ef0270ea69c5e0b9bcadcd8b0f842ca158c7
Pranay Singh <pranay.singh.1393@gmail.com>
2021-11-18 22:25:14 +0530

    Test 1
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Test1/src/NumberStarPattern1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumberStarPattern1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = n; j >= 1; j--) {
+				if(i==j) {
+					System.out.print("*");
+					continue;
+				}
+				System.out.print(j);
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
