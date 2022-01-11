2b32ae5896ed343c47d191f003b2f1c8809474d5
Pranay Singh <pranay.singh.1393@gmail.com>
2022-01-11 18:16:24 +0530

    Taking Input and Print 2DArray
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Lecture12/src/twoDimensionalArrays/InputPrint2DArray.java
@@ -0,0 +1,34 @@
+package twoDimensionalArrays;
+
+import java.util.Scanner;
+
+public class InputPrint2DArray {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no. of rows: ");
+		int rows = sc.nextInt();
+		System.out.println("Enter the no. of columns: ");
+		int cols = sc.nextInt();
+
+		int arr[][] = new int[rows][cols];
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				System.out.println("Enter the value for " + i + " row and " + j + " column ");
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
