213ba40aebaebf854d9dcdf8ce21b6a88994ed07
Pranay Singh <pranay.singh.1393@gmail.com>
2022-01-13 04:10:03 +0530

    Access Modifier

---
+++ Lecture13_OOPS1/src/oops/accessModifiers/AccessModifier.java
@@ -0,0 +1,15 @@
+package oops.accessModifiers;
+
+public class AccessModifier {
+
+	@SuppressWarnings("unused")
+	private int id;
+	String name;
+	protected String address;
+	public int age;
+
+}
+
+//public class AccessModifierChild extends AccessModifier {
+//
+//}
---
+++ Lecture13_OOPS1/src/oops/accessModifiers/AccessModifiersTest.java
@@ -0,0 +1,16 @@
+package oops.accessModifiers;
+
+public class AccessModifiersTest {
+
+	public static void main(String[] args) {
+		
+		AccessModifier am=new AccessModifier();
+		am.name="Pranay";
+		am.age=28;
+		am.address="Indore";
+		// am.id//not access
+				
+
+	}
+
+}
