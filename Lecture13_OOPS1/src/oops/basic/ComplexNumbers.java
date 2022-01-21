103817037f442cfa46b9a5c1421d3facf1a14279
Pranay Singh <pranay.singh.1393@gmail.com>
2022-01-21 19:47:07 +0530

    OOPs basic Complex Numbers

---
+++ Lecture13_OOPS1/src/oops/basic/ComplexNumbers.java
@@ -0,0 +1,28 @@
+package oops.basic;
+
+public class ComplexNumbers {
+
+	private int real;
+	private int imaginary;
+
+	public ComplexNumbers(int real, int imaginary) {
+		this.real = real;
+		this.imaginary = imaginary;
+	}
+
+	public void plus(ComplexNumbers c) {
+		this.real = this.real + c.real;
+		this.imaginary = this.imaginary + c.imaginary;
+	}
+
+	public void multiply(ComplexNumbers c) {
+		int temp = this.real;
+		this.real = this.real * c.real - this.imaginary * c.imaginary;
+		this.imaginary = temp * c.imaginary + this.imaginary * c.real;
+	}
+
+	public void print() {
+		System.out.println(this.real + " + " + "i" + this.imaginary);
+	}
+
+}
