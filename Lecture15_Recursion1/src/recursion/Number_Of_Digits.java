4ed00abfc12dc9ef231aac7cd7eb953647166e3c
Pranay Singh <pranay.singh.1393@gmail.com>
2022-02-16 23:19:06 +0530

    Display Number Of Digits Using Recursion
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Lecture15_Recursion1/src/recursion/Number_Of_Digits.java
@@ -0,0 +1,25 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class Number_Of_Digits {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(Solution1.count(n));
+	}
+
+}
+
+class Solution1 {
+
+	public static int count(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		int smallAns = count(n / 10);
+		return smallAns + 1;
+	}
+}
