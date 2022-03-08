888232f927656aa62470b614de633b57b3c143f3
Pranay Singh <pranay.singh.1393@gmail.com>
2022-03-09 05:06:32 +0530

    Final Keyword
    Signed-off-by: Pranay Singh <pranay.singh.1393@gmail.com>

---
+++ Lecture18_OOPS3/src/oops/finalKeyword/Final_Keyword.java
@@ -0,0 +1,53 @@
+package oops.finalKeyword;
+
+/*
+1) Java final variable
+If you make any variable as final, you cannot change the value of final variable(It will be constant).
+*/
+
+/*
+2) Java final method
+If you make any method as final, you cannot override it.
+*/
+
+/*
+3) Java final class
+If you make any class as final, you cannot extend it.
+*/
+
+class Pen {
+
+	final int price = 15;
+
+	final String color;// blank final variable
+
+	Pen() {
+		this.color = "Red";// we can initialize blank final variable but only in constructor.
+	}
+
+	final void run() {
+		System.out.println("writing");
+	}
+}
+
+//final class Pencil{
+//	
+//}
+
+public class Final_Keyword extends Pen {
+
+//	void run() {
+//		System.out.println("writing safely ");
+//	}
+
+	public static void main(String[] args) {
+		Pen p = new Pen();
+		// p.price=20; // Compilation Error as price is final
+		System.out.println(p.price);
+
+		// final method is inherited but you cannot override it.
+		p.run();
+		System.out.println(p.color);
+	}
+
+}
